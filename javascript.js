d3c4cc6e3301350e6eff8f8da1159b5097b3a5f1
Ian <imacleod@mail.umw.edu>
2022-07-05 10:09:52 -0400

    Work on calculations for temp conversions

--- javascript.js
+++ javascript.js
@@ -2,12 +2,15 @@ let temperature = document.getElementById("temp");
 let conversion = document.querySelector(".result");
 
 let f = document.getElementById("fah");
-let c = document.getElementById("cel");
-console.dir(f);
 
 function convert() {
     if (f.checked) {
-        let newTemp = (parseInt(temp.value) - 32) * (5 / 9);
+        let newTemp = (parseFloat(temp.value) - 32) * (5 / 9);
         conversion.innerHTML = newTemp;
+        document.querySelector(".text").innerHTML = " Degrees Celsius";
+    } else {
+        let newTemp = (parseFloat(temp.value) * 9 / 5) + 32;
+        conversion.innerHTML = newTemp;
+        document.querySelector(".text").innerHTML = " Degrees Fahrenheit";
     }
 }
